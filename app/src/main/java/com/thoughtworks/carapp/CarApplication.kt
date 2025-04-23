bbcead228160ee7eb41c15527fd96fa526191948
xiaolu.huo <xiaolu.huo@thoughtworks.com>
2025-04-23 10:59:22 +0800

    feat: change carService connection to application

--- app/src/main/java/com/thoughtworks/carapp/CarApplication.kt
+++ app/src/main/java/com/thoughtworks/carapp/CarApplication.kt
@@ -1,9 +1,24 @@
 package com.thoughtworks.carapp
 
 import android.app.Application
+import com.thoughtworks.carapp.service.CarService
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class CarApplication : Application() {
 
+    @Inject
+    lateinit var carService: CarService // 触发注入
+
+    override fun onCreate() {
+        super.onCreate()
+        // 访问 carService 确保实例化（可选，依赖注入已自动完成）
+        carService.connect()
+    }
+
+    override fun onTerminate() {
+        carService.disconnect()
+        super.onTerminate()
+    }
 }
--- app/src/main/java/com/thoughtworks/carapp/ui/main/viewmodel/CarViewModel.kt
+++ app/src/main/java/com/thoughtworks/carapp/ui/main/viewmodel/CarViewModel.kt
@@ -1,17 +1,13 @@
 package com.thoughtworks.carapp.ui.main.viewmodel
 
-import android.app.Application
 import android.car.VehicleIgnitionState
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.thoughtworks.carapp.service.CarService
 import com.thoughtworks.carapp.ui.main.Toggle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -26,7 +22,6 @@ class CarViewModel @Inject constructor(
     }
 
     private fun connectToCar() {
-        carService.connect()
         carService.registerIgnitionStateListener { state ->
             _engineState.value = if (state == VehicleIgnitionState.ON) Toggle.On else Toggle.Off
         }
@@ -35,6 +30,5 @@ class CarViewModel @Inject constructor(
     override fun onCleared() {
         super.onCleared()
         carService.unregisterIgnitionStateListener()
-        carService.disconnect()
     }
 } 
