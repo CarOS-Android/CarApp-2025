2485eaa24f18eebb2a98ecc759057f2f42201e1e
Jian Zhang <jizhang@thoughtworks.com>
2025-04-23 15:54:05 +0800

    feat: merge properties listening into car service

--- app/src/main/java/com/thoughtworks/carapp/service/CarService.kt
+++ app/src/main/java/com/thoughtworks/carapp/service/CarService.kt
@@ -4,6 +4,8 @@ import android.car.Car
 import android.car.VehiclePropertyIds
 import android.car.hardware.CarPropertyValue
 import android.car.hardware.property.CarPropertyManager
+import android.car.hardware.property.CarPropertyManager.CarPropertyEventCallback
+import android.car.hardware.property.Subscription
 import android.content.Context
 import android.util.Log
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -17,6 +19,7 @@ class CarService @Inject constructor(
     private var car: Car? = null
     private var carPropertyManager: CarPropertyManager? = null
     private var ignitionStateChangeListener: ((Int) -> Unit)? = null
+    private var propertyCallbacks: MutableList<Map<Int, (Any) -> Unit>> = mutableListOf()
 
     fun connect() {
         car = Car.createCar(context)
@@ -36,6 +39,33 @@ class CarService @Inject constructor(
             CarPropertyManager.SENSOR_RATE_ONCHANGE,
             ignitionCallback
         )
+        carPropertyManager?.subscribePropertyEvents(
+            VehiclePropertyIds.IGNITION_STATE,
+            CarPropertyManager.SENSOR_RATE_ONCHANGE,
+            ignitionCallback
+        )
+    }
+
+    fun registerPropertyListeners(callbacks: List<Map<Int, (Any) -> Unit>>) {
+        val subscriptions: List<Subscription> = callbacks.flatMap { callback ->
+            callback.keys.map { propertyId ->
+                Subscription.Builder(propertyId)
+                    .build()
+            }
+        }
+
+        carPropertyManager?.subscribePropertyEvents(subscriptions, null, commonCallback)
+        propertyCallbacks.addAll(callbacks)
+    }
+
+    fun unregisterPropertyListeners(callbacks: List<Map<Int, (Any) -> Unit>>) {
+        callbacks.forEach { callback ->
+            propertyCallbacks.remove(callback)
+        }
+
+        if(propertyCallbacks.size == 0) {
+            carPropertyManager?.unsubscribePropertyEvents(commonCallback)
+        }
     }
 
     fun unregisterIgnitionStateListener() {
@@ -48,6 +78,18 @@ class CarService @Inject constructor(
             if (value.propertyId == VehiclePropertyIds.IGNITION_STATE) {
                 val state = value.value as Int
                 ignitionStateChangeListener?.invoke(state)
+            }
+        }
+
+        override fun onErrorEvent(propId: Int, zone: Int) {
+            Log.e("CarService", "Error listening to IGNITION_STATE")
+        }
+    }
+
+    private val commonCallback = object : CarPropertyManager.CarPropertyEventCallback {
+        override fun onChangeEvent(value: CarPropertyValue<Any?>) {
+            propertyCallbacks.forEach { callback ->
+                callback[value.propertyId]?.invoke(value.value)
             }
         }
 
--- app/src/main/java/com/thoughtworks/carapp/ui/main/viewmodel/CarViewModel.kt
+++ app/src/main/java/com/thoughtworks/carapp/ui/main/viewmodel/CarViewModel.kt
@@ -1,6 +1,7 @@
 package com.thoughtworks.carapp.ui.main.viewmodel
 
 import android.car.VehicleIgnitionState
+import android.car.VehiclePropertyIds
 import androidx.lifecycle.ViewModel
 import com.thoughtworks.carapp.service.CarService
 import com.thoughtworks.carapp.ui.main.Toggle
@@ -20,18 +21,30 @@ class CarViewModel @Inject constructor(
     private val _autoHoldState = MutableStateFlow(Toggle.Off)
     val autoHoldState: StateFlow<Toggle> = _autoHoldState.asStateFlow()
 
+    private var propertyCallbacks: List<Map<Int, (Any) -> Unit>> = mutableListOf()
+
     init {
         connectToCar()
     }
 
     private fun connectToCar() {
-        carService.registerIgnitionStateListener { state ->
-            _engineState.value = if (state == VehicleIgnitionState.ON) Toggle.On else Toggle.Off
-        }
+        this.propertyCallbacks = listOf<Map<Int, (Any) -> Unit>>(
+            mapOf<Int, (Any) -> Unit>(
+                VehiclePropertyIds.IGNITION_STATE to { value ->
+                    _engineState.value = if (value == VehicleIgnitionState.ON) Toggle.On else Toggle.Off
+                },
+                VehiclePropertyIds.PARKING_BRAKE_AUTO_APPLY to { value ->
+                    _autoHoldState.value = if (value as? Boolean == true) Toggle.On else Toggle.Off
+                },
+                VehiclePropertyIds.PARKING_BRAKE_ON to { value ->
+
+                })
+        )
+        carService.registerPropertyListeners(this.propertyCallbacks)
     }
 
     override fun onCleared() {
         super.onCleared()
-        carService.unregisterIgnitionStateListener()
+        carService.unregisterPropertyListeners(this.propertyCallbacks)
     }
-} 
+}
